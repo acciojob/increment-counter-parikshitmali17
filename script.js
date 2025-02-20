e9e68939205e2bb32d71da01d5380e7ed7e0dda5
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-20 16:20:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let p= document.querySelector("#counter")
+let i=0;
+document.querySelector("#incrementBtn").addEventListener('click',()=>{
+	alert(i);
+	i++
+	p.innerText=i
+})
